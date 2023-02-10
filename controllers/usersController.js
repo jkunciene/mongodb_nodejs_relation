962fcef5cf58c8cf9c4d963ef9a8be61310e184a
Mokytojas <Mokytojas@kitm.local>
2023-02-10 12:49:08 +0200

    post metodas veikia

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,8 +1,19 @@
 const User = require('../models/User');
 
 const createUser = async(req, res)=>{
-    
 
-    // name: String,
-    // about: String
+    if(!req.body.name || !req.body.about) res.status(404).send("Not Found");
+
+    const user = new User({
+        name: req.body.name,
+        about: req.body.about
+    });
+
+    const result = await user.save();
+    res.status(200).send(result);
+
+}
+
+module.exports = {
+    createUser
 }
--- index.js
+++ index.js
@@ -7,3 +7,10 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const { createUser } = require('./controllers/usersController');
+
+app.post('/api/user', createUser);
+
+app.listen(process.env.PORT, ()=> {
+    console.log(`Server is running on PORT ${process.env.PORT}`)
+});
